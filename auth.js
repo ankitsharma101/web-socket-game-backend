e1db9047296b8ec73cbbc8907463d42d95fa6528
ankitsharma101 <ankitsharma101.exe@gmail.com>
2024-08-30 16:23:14 +0530

    Applied authentication and fixed the redirecting routes

---
+++ auth.js
@@ -0,0 +1,29 @@
+// backend/auth.js
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.get('/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect(FRONTEND_URL); // Assuming your React app runs on port 3000
+  }
+);
+
+router.get('/logout', (req, res) => {
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect(FRONTEND_URL);
+  });
+});
+
+router.get('/current_user', (req, res) => {
+  res.send(req.user); // Send the authenticated user to the frontend
+});
+
+module.exports = router;
---
+++ user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  googleId: String,
+  name: String,
+  email: String,
+  avatar: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
